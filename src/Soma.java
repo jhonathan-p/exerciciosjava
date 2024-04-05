d995a0f4903c5fccb3773d1d7a0a4653266c4899
Jhonathan Pfeiffer Urbainski <jhonathan.pfeiffer@gmail.com>
2024-04-05 01:53:33 -0300

    Initial commit

---
+++ src/Soma.java
@@ -0,0 +1,9 @@
+public class Soma {
+    public static void main(String[] args) {
+
+        int primeironumero = 10;
+        int segundonumero = 5;
+        System.out.println(primeironumero + segundonumero);
+
+    }
+}
