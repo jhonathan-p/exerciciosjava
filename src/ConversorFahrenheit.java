039adf792237449a85934a1fe457b718113015c2
Jhonathan Pfeiffer Urbainski <jhonathan.pfeiffer@gmail.com>
2024-04-06 01:32:33 -0300

    Exercício ConversorFahrenheit.java e atualização de Estudos.java

---
+++ src/ConversorFahrenheit.java
@@ -0,0 +1,14 @@
+public class ConversorFahrenheit {
+    public static void main(String[] args) {
+
+        double celsius = 19;
+        double fahrenheit = (celsius * 1.8) + 32;
+
+        System.out.println(fahrenheit);
+
+        int fahrenheitSemDecimais = (int) fahrenheit;
+
+        System.out.println(fahrenheitSemDecimais);
+
+    }
+}
--- src/Estudos.java
+++ src/Estudos.java
@@ -4,6 +4,134 @@ public class Estudos {
         String link = "https://www.youtube.com/watch?v=GUanHEGlje4&t";
         System.out.println("Estou estudando Java. Gostei muito do vídeo a seguir:");
         System.out.println(link);
+        char meuChar = '♥';
+        System.out.println(meuChar);
+        System.out.println("Abaixo tenho algumas anotações interessantes:");
+
+        System.out.println("""
+        
+        /*
+        boolean (true ou false)
+        byte (8 bits, -128 a 127)
+        char (caracteres individuais, qualquer Unicode, precisa de aspas simples '')
+        short (16 bits, -32.768 a 32.767)
+        int (32 bits, -2.147.483.648 a 2.147.483.647)
+        long (64 bits, -9.223.372.036.854.775.808 a 9.223.372.036.854.775.807)
+        float (32 bits, até 7 digitos de precisão)
+        double (64 bits, até 15 digitos de precisão)
+        */
+        
+        
+        
+        Casting Implícito (conversão automática) vs Casting Explícito (conversão forçada, pode perder dados):
+        Se o tipo de dado a ser convertido for menor do que quem recebe, geralmente é possível converter sem precisar declarar, exceto para o tipo char.
+        Hierarquia: byte > short > char > int > long > float > double.
+        Exemplo:
+        byte converte para qualquer coisa de forma implícita, exceto char.
+        double é sempre preciso explicitar ao converter para outro tipo.
+            
+            int x = 10;
+            double y = x; // casting implícito
+            
+            double x = 10.5;
+            int y = (int) x; // casting explícito
+        
+
+
+
+        + (adição)
+        - (subtração)
+        * (multiplicação)
+        / (divisão)
+        % (resto da divisão)
+        
+
+        
+        == (igual a)
+        != (diferente de)
+        > (maior que)
+        >= (maior ou igual a)
+        < (menor que)
+        <= (menor ou igual a)
+        
+
+        
+        && (AND) (Duas ou mais condições precisam ser verdadeiras)
+        || (OR) (Pelo menos uma condição precisa ser verdadeira)
+        ! (NOT) (Nega a condição, se for verdadeiro o código não é executado)
+        
+
+        
+        ++num
+            int num = 5;
+            int resultado = ++num; //num é incrementado para 6 e depois atribuído a resultado
+            System.out.println(num); // imprime 6
+            System.out.println(resultado); // imprime 6
+        
+        num++
+            int num = 5;
+            int resultado = num++; //num é atribuído primeiramente à variável resultado e depois incrementado para 6
+            System.out.println(num); // imprime 6
+            System.out.println(resultado); // imprime 5 
+        
+        
+        
+        Comparar Strings com == compara o mesmo objeto na memória e não o conteúdo das Strings.        
+        Para comparar o conteúdo de duas Strings, é necessário utilizar o método equals().
+        Existe também o equalsIgnoreCase() que ignora distinção de letras maiúsculas e minúsculas.
+        Exemplo:
+            String senha = "12345";
+            if (senha.equals("12345")) {
+                System.out.println("Acesso autorizado!");
+            } else {
+                System.out.println("Senha incorreta.");
+            }
+            
+            
+            
+        Método format(), da classe String serve para formatar Strings (String.format("%s %d %f")).
+        Placeholders:
+        %s (string)
+        %d (dígito)
+        %f (float) (%.2f mostra apenas duas casas decimais)
+        Exemplo:
+            String nome = "Maria";
+            int idade = 30;
+            double valor = 55.9999;
+            System.out.println(String.format("Meu nome é %s, eu tenho %d anos e hoje gastei %.2f reais", nome, idade, valor));
+            
+        É possível também usar o método formatted() com Text Block.
+        Exemplo:
+            String nome = "João";
+            int aulas = 4;                    
+            String mensagem = \"""
+                              Olá, %s!
+                              Boas vindos ao curso de Java.
+                              Teremos %d aulas para te mostrar o que é preciso para você dar o seu primeiro mergulho na linguagem!
+                              \""".formatted(nome, aulas);
+            System.out.println(mensagem);
+
+
+
+        Nomes de classes devem começar com letra maiúscula e usar a convenção PascalCase (também conhecida como Upper CamelCase).
+        Exemplo: MinhaClasse.
+
+        Nomes de métodos devem começar com letra minúscula e usar a convenção camelCase.
+        Exemplo: meuMetodo().
+
+        Nomes de constantes devem ser totalmente em letras maiúsculas, separadas por underline.
+        Exemplo: MINHA_CONSTANTE.
+
+        Nomes de variáveis devem começar com letra minúscula e usar a convenção camelCase.
+        Exemplo: minhaVariavel.
+
+        Todas as linhas de código devem ter no máximo 80 caracteres de largura para facilitar a leitura.
+
+        Recomenda-se usar espaços em branco para separar operadores, palavras-chave e elementos de controle de fluxo.
+        Exemplo: if (condicao) {.
+
+        Use comentários para documentar seu código, explicando o que ele faz e por que ele faz isso. Comentários devem ser claros e concisos.
+        """);
 
     }
 }
